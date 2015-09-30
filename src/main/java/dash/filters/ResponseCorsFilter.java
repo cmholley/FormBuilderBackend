775057a04ad696e685d84a48ac745374ba9bb7c1
Chris Holley <cmholley97@gmail.com>
2015-09-29 22:47:25 -0500

    Reading from two tenants is working with hard coded tenant ID.

--- src/main/java/dash/filters/ResponseCorsFilter.java
+++ src/main/java/dash/filters/ResponseCorsFilter.java
@@ -19,60 +19,68 @@ import javax.servlet.http.HttpServletResponse;
 public class ResponseCorsFilter implements Filter {
 
 	@Override
-	public void init(final FilterConfig filterConfig) throws ServletException { }
+	public void init(final FilterConfig filterConfig) throws ServletException {
+	}
 
 	@Override
-	public void destroy() { }
+	public void destroy() {
+	}
 
 	@Override
-	public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+	public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse,
+			final FilterChain filterChain) throws IOException, ServletException {
 		if (servletResponse instanceof HttpServletResponse) {
-	        
+
 			final HttpServletResponse alteredResponse = ((HttpServletResponse) servletResponse);
-			
+
 			// Get client's origin
 			HttpServletRequest origRequest = (HttpServletRequest) servletRequest;
-			
-			//try to match origin with list
-			String matchedOrigin = matchOrigin(origRequest.getHeader("origin"));
-			if (matchedOrigin != null) {
-				//matched origin will be attached to response header
-				addHeadersFor200Response(alteredResponse, matchedOrigin);
-			} else {
-				//fall back to "*"
-				addHeadersFor200Response(alteredResponse, "*");
+
+			String originHeader = origRequest.getHeader("origin");
+			// try to match origin with list
+			if (originHeader != null) {
+
+				String matchedOrigin = matchOrigin(origRequest.getHeader("origin"));
+				if (matchedOrigin != null) {
+					// matched origin will be attached to response header
+					addHeadersFor200Response(alteredResponse, matchedOrigin);
+				} else {
+					// fall back to "*"
+					addHeadersFor200Response(alteredResponse, "*");
+				}
 			}
 		}
 		filterChain.doFilter(servletRequest, servletResponse);
 	}
-	
+
 	/**
-	 * Trying to match the incoming request with a list of domain names. If the origin of the request is in the list it 
-	 * will return the same origin, otherwise the return will be null.
+	 * Trying to match the incoming request with a list of domain names. If the
+	 * origin of the request is in the list it will return the same origin,
+	 * otherwise the return will be null.
 	 * 
-	 * @param origin The request origin.
+	 * @param origin
+	 *            The request origin.
 	 * @return
 	 */
 	private String matchOrigin(String origin) {
-		
-		//list of origins to match
+
+		// list of origins to match
 		if (!origin.matches("http://www.housuggest.org|http://housuggest.org|http://localhost:8100"))
 			return null;
-		
-	  return origin;
+
+		return origin;
 	}
-	
+
 	private void addHeadersFor200Response(final HttpServletResponse response, String origin) {
 		response.addHeader("Access-Control-Allow-Origin", origin);
-		
-		response.addHeader("Access-Control-Allow-Methods",
-				"GET, POST, PUT, OPTIONS, DELETE");
+
+		response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
 		response.addHeader("Access-Control-Allow-Credentials", "true");
-		response.addHeader(
-				"Access-Control-Allow-Headers",
+		response.addHeader("Access-Control-Allow-Headers",
 				"Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, X-XSRF-TOKEN");
-		
-		//Exposes all headers so that Restangular client may retrieve data from it.
-		//response.addHeader("Access-Control-Expose-Headers", "ObjectId");
+
+		// Exposes all headers so that Restangular client may retrieve data from
+		// it.
+		// response.addHeader("Access-Control-Expose-Headers", "ObjectId");
 	}
 }
